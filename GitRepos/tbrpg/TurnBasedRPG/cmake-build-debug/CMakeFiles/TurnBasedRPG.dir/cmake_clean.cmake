dcdd30a5764bc892994997a8987ec72464f148eb
Arturan <Arturan@DESKTOP-AQH8SND>
2021-03-13 18:34:09 +0200

    project setup

---
+++ GitRepos/tbrpg/TurnBasedRPG/cmake-build-debug/CMakeFiles/TurnBasedRPG.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TurnBasedRPG.dir/Src/main.cpp.obj"
+  "TurnBasedRPG.exe"
+  "TurnBasedRPG.exe.manifest"
+  "TurnBasedRPG.pdb"
+  "libTurnBasedRPG.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TurnBasedRPG.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
