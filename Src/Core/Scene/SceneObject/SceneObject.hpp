7ae221298ce5823d75cbf93f57f4fe03cdcc8980
aijey <artur.sydoran@gmail.com>
2021-03-18 01:19:43 +0200

    DelayedEvent added Scene removeSceneObject added

--- Src/Core/Scene/SceneObject/SceneObject.hpp
+++ Src/Core/Scene/SceneObject/SceneObject.hpp
@@ -6,30 +6,39 @@
 #define TBRPG_SCENEOBJECT_HPP
 
 #include <memory>
+#include <SFML/Graphics.hpp>
 #include "Transform/Transform.hpp"
-#include "../Component/VisualComponent/VisualComponent.hpp"
-#include "../Component/CameraComponent/CameraComponent.hpp"
 
-
+class VisualComponent;
+class CameraComponent;
 class Scene;
 class SceneObject {
+    friend class Scene;
 public:
     Transform transform;
-    std::vector<std::shared_ptr<VisualComponent>> visualComponents;
-    std::vector<std::shared_ptr<CameraComponent>> cameraComponents;
     std::string name;
     explicit SceneObject(const std::string& name, Scene& scene);
-    void createVisualComponent(std::shared_ptr<sf::Drawable> drawable);
-    void createVisualComponent(std::shared_ptr<sf::Drawable> drawable, const sf::Vector2f& pivot);
-    void copyVisualComponent(const std::shared_ptr<VisualComponent>& visualComponent);
-    void createCameraComponent(std::shared_ptr<sf::RenderWindow> renderWindow);
-    void copyCameraComponent(const std::shared_ptr<CameraComponent>& cameraComponent);
     SceneObject(const SceneObject& sceneObject) = delete;
-    std::shared_ptr<SceneObject> copy() const;
 
-private:
+    void createVisualComponent(const std::shared_ptr<sf::Drawable>& drawable);
+    void createVisualComponent(const std::shared_ptr<sf::Drawable>& drawable, const sf::Vector2f& pivot);
+    void removeVisualComponent(std::shared_ptr<VisualComponent>& visualComponent);
+
+    void createCameraComponent(std::shared_ptr<sf::RenderWindow>& renderWindow);
+    void removeCameraComponent(std::shared_ptr<CameraComponent>& cameraComponent);
+
+    std::shared_ptr<SceneObject> copy() const;
+protected:
     Scene& scene;
+    std::vector<std::shared_ptr<VisualComponent>> visualComponents;
+    std::vector<std::shared_ptr<CameraComponent>> cameraComponents;
+    void copyCameraComponent(const std::shared_ptr<CameraComponent>& cameraComponent);
+    void copyVisualComponent(const std::shared_ptr<VisualComponent>& visualComponent);
+    template<class T>
+    void removeComponent(std::vector<std::shared_ptr<T>>& from, std::shared_ptr<T>& component);
 };
+
+
 
 
 #endif //TBRPG_SCENEOBJECT_HPP
